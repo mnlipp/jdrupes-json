399a7835bde9aebdb49dd723417427e5e0ec67ff
Michael N. Lipp <mnl@mnl.de>
2021-11-07 17:21:32 +0100

    Merge changes from no-mxbeans branch.

--- src/org/jdrupes/json/JsonBeanDecoder.java
+++ src/org/jdrupes/json/JsonBeanDecoder.java
@@ -129,6 +129,7 @@ public class JsonBeanDecoder extends JsonCodec {
     private static final Object END_VALUE = new Object();
     @SuppressWarnings("PMD.UseConcurrentHashMap")
     private final Map<String, Class<?>> aliases = new HashMap<>();
+    private boolean skipUnknown = false;
     private Function<String, Optional<Class<
             ?>>> classConverter = name -> {
                 try {
@@ -164,6 +165,20 @@ public class JsonBeanDecoder extends JsonCodec {
     public JsonBeanDecoder setClassConverter(
             Function<String, Optional<Class<?>>> converter) {
         this.classConverter = converter;
+        return this;
+    }
+
+    /**
+     * Cause this decoder to silently skip information from the JSON source
+     * that cannot be mapped to a property of the bean being created.
+     * This is useful if e.g. a REST invocation returns data that you
+     * are not interested in and therefore don't want to model in your 
+     * JavaBean. 
+     * 
+     * @return the decoder for chaining
+     */
+    public JsonBeanDecoder skipUnknown() {
+        skipUnknown = true;
         return this;
     }
 
@@ -601,6 +616,9 @@ public class JsonBeanDecoder extends JsonCodec {
         for (Map.Entry<String, ?> e : propsMap.entrySet()) {
             PropertyDescriptor property = beanProps.get(e.getKey());
             if (property == null) {
+                if (skipUnknown) {
+                    continue;
+                }
                 throw new JsonDecodeException(parser.getCurrentLocation()
                     + ": No bean property for key " + e.getKey());
             }
@@ -664,11 +682,12 @@ public class JsonBeanDecoder extends JsonCodec {
             case FIELD_NAME:
                 String key = parser.getText();
                 PropertyDescriptor property = beanProps.get(key);
+                Object value;
                 if (property == null) {
-                    throw new JsonDecodeException(parser.getCurrentLocation()
-                        + ": No bean property for key " + key);
+                    value = readValue(Object.class, null);
+                } else {
+                    value = readValue(property.getPropertyType(), null);
                 }
-                Object value = readValue(property.getPropertyType(), null);
                 map.put(key, value);
                 break;
 
